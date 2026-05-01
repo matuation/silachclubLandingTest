50f1d850129a0688bdf298468186f4617321e51b
Alexey Schastlivtsev <matuation@gmail.com>
2026-05-01 18:07:45 +0300

    video and log fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -55,6 +55,11 @@ public class TestBase {
         Attachments.browserConsoleLogs();
         String sessionId = Selenide.sessionId().toString();
         closeWebDriver();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         Attachments.addVideo(sessionId);
 
     }
