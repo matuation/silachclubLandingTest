02dac297f56fee80d195589937a3449cba32cdd8
Alexey Schastlivtsev <matuation@gmail.com>
2026-05-01 21:22:56 +0300

    video and log fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -61,7 +61,7 @@ public class TestBase {
         closeWebDriver();
         if (config.isRemote()) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(10000);
             } catch (InterruptedException ignored) {
             }
             Attachments.addVideo(sessionId);
