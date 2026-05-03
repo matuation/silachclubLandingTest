170d87e652405686eccdb625cc37ebb17da3d782
Alexey Schastlivtsev <matuation@gmail.com>
2026-05-03 21:37:49 +0300

    selenoid properties fix

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -3,7 +3,7 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources({
 
-        "classpath:${env}.properties",
+        "classpath:config/${env}.properties",
 })
 
 public interface WebConfig extends Config {
